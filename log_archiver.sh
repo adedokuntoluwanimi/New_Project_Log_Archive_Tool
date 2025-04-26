f036a797b1647b2d86b89f0730ea83e271398f4b
Adedokun Toluwanimi <adedokuntoluwanimi@mtu.edu.ng>
2025-04-26 20:06:52 +0100

    Add log_archiver.sh script

---
+++ log_archiver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Defining where the logs are and where to save the archives
+
+LOG_DIR="./logs"      #Folder where the logs are
+ARCHIVE_DIR="./archives" #Folder where the archives would be stored
+
+#Creating the archive folder
+mkdir -p "$ARCHIVE_DIR"
+
+
+#Creating Archive Name Using Current Date and Time
+CURRENT_TIME=$(date + "%Y-%m-%d_%H-%M-%S")
+ARCHIVE_NAME="logs_$CURRENT_TIME.tar.gz"
+
+
+#Compressing logs into the archive
+tar -czvf "$ARCHIVE_DIR/$ARCHIVE_NAME" "$LOG_DIR"
+
+echo "Logs have been archived successfully into $ARCHIVE_DIR/$ARCHIVE_NAME
+
